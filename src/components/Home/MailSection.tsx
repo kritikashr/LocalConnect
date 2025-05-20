b9a5773f6bc160ceed2a35400a3e6b9825ff90d9
Summer <kritika.shrestha003@gmail.com>
2025-05-20 11:11:52 +0545

    figma file added

--- src/components/Home/MailSection.tsx
+++ src/components/Home/MailSection.tsx
@@ -5,7 +5,7 @@ const MailSection = () => {
     <div className="flex flex-col justify-center items-center gap-5 mt-20 py-14">
       <h2>Stay Updated</h2>
       <form action="">
-        <input type="mail" placeholder='Enter your mail' />
+        <input type="email" placeholder='Enter your mail' />
         <input type="submit" placeholder='Subscribe' name='Subscribe'/>
       </form>
     </div>
