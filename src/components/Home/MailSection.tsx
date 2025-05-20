0fc9131ba4baa0a41751dc8f57dd6bcf4c49581e
Summer <kritika.shrestha003@gmail.com>
2025-05-20 16:29:43 +0545

    Email Form

--- src/components/Home/MailSection.tsx
+++ src/components/Home/MailSection.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
+import Email from '../Form/Email'
 
 const MailSection = () => {
   return (
     <div className="flex flex-col justify-center items-center gap-5 mt-20 py-14">
       <h2>Stay Updated</h2>
-      <form action="">
-        <input type="email" placeholder='Enter your mail' />
-        <input type="submit" placeholder='Subscribe' name='Subscribe'/>
-      </form>
+      <Email/>
     </div>
   )
 }
