e65bb0d21ae8d34467692d5af1d9d67203961242
Summer <kritika.shrestha003@gmail.com>
2025-06-02 09:47:26 +0545

    Complaint page

--- src/app/complaint/page.tsx
+++ src/app/complaint/page.tsx
@@ -1,7 +1,8 @@
+import ComplaintCard from "@/components/Complaint/ComplaintCard";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return <div><ComplaintCard/></div>;
 };
 
 export default page;
